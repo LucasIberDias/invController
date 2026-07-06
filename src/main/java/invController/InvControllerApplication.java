8b24596cc912e124eefb072a0c0999336983f579
LucasIberDias <lucas.skuggar@gmail.com>
2026-07-06 08:25:01 -0300

    Initial commit

---
+++ src/main/java/invController/InvControllerApplication.java
@@ -0,0 +1,13 @@
+package invController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/invController/InvControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package invController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
