f3fed99ce2ab6b3c057c37f77b8f1f588990e119
LucasIberDias <lucas.skuggar@gmail.com>
2026-07-10 09:41:26 -0300

    frontend

---
+++ front-end/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-100">
+        <div className="bg-white p-8 rounded-xl shadow-lg">
+          <h1 className="text-3xl font-bold text-blue-600">
+            InvController 🚀
+          </h1>
+
+          <p className="mt-2 text-slate-600">
+            Tailwind funcionando com sucesso!
+          </p>
+
+          <button className="mt-6 bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+            Começar
+          </button>
+        </div>
+      </div>
+  );
+}
+
+export default App;
